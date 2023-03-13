14a79b7722cd47c40b3bb6bdc9c63dd596f96137
mathrip <mathilde.ripart@gmail.com>
2023-03-13 15:44:38 +0000

    update version

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ try:
 except ImportError:
     __author__ = __maintainer__ = "MELD development team"
     __email__ = "meld.study@gmail.com"
-    __version__ = "1.0.1"
+    __version__ = "1.1.2"
 
 setup(
     name="meld_classifier",
-    version="1.0.1",
+    version="1.1.2",
     packages=find_packages(),
     package_dir={"meld_classifier": "meld_classifier"},
 )
